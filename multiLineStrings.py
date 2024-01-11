0530b7e381ae8b4021f8dd0f1f14ced028986f70
amansingh202 <hellraisertelevision@gmail.com>
2024-01-11 01:22:16 -0600

    multi line strings learning

---
+++ multiLineStrings.py
@@ -0,0 +1,15 @@
+myString = '''
+This is used to make multi line strings World War II or the Second World 
+War[b] was a global conflict that lasted from 1939 to 1945. The vast majority of the world's countries, 
+including all the great powers, fought as part of two opposing military alliances: the Allies and the Axis. 
+Many participating countries invested all available economic, industrial, and scientific capabilities into this total war, 
+blurring the distinction between civilian and military resources. Aircraft played a major role, enabling the strategic 
+bombing of population centres and delivery of the only two nuclear weapons ever used in war. It was by far the 
+deadliest conflict in history, 
+'''
+
+print(myString)
+
+city = "Stalingrad"
+
+print("%s battle was the deadliest" % city)
